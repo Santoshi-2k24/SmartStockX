b921902e6e88c880822e649a7257f5429d53d53d
Santoshi-2k24 <santoshisambhumahanti@gmail.com>
2025-07-11 20:13:54 +0530

    Update settings.py

--- Backend/SmartStockX/SmartStockX/settings.py
+++ Backend/SmartStockX/SmartStockX/settings.py
@@ -74,10 +74,11 @@ ROOT_URLCONF = 'SmartStockX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'SmartStockX', 'templates')],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
